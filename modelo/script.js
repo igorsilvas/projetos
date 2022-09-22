5d29eea125ef051c2a51541471fd0537b2cad99e
Ygor Silva <113484193+igorsilvas@users.noreply.github.com>
2022-09-21 23:32:33 -0300

    javascript

--- modelo/script.js
+++ modelo/script.js
@@ -4,5 +4,15 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'imagens/manha.png'
+    } else if(hora >= 12 && hora < 18) {
+        //BOA TARDE!
+        img.src = 'imagens/tarde.png'
+    } else {
+        //BOA NOITE!
+        img.src = 'imagens/noite.png'
+    }
 
 }
